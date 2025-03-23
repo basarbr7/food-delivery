e1acf83804cf9485929ac4cfa73969b3a3144a7f
M.A. Basar <basarbr7@gmail.com>
2025-03-23 12:27:23 +0600

    added google font

---
+++ src/component/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Container from '../layer/Container'
+
+const Header = () => {
+  return (
+    <div>
+        <Container>
+            <p> Get 5% Off your first order, <span>Promo: ORDER5</span></p>
+        </Container>
+    </div>
+  )
+}
+
+export default Header
